52cb462ed25ba16690b19ffe34da278f7fc37b08
ssongire <ssongire@umail.iu.edu>
2016-09-08 02:12:37 -0400

    added initial authentication with Auth0

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team Omega' });
+  var toSend = {
+    title: 'Express',
+    env: env
+  }
+  res.render('index', {tosend : toSend});
 });
+
+router.get('/login',
+  function(req, res){
+    var toSend = {
+    env: env
+  }
+    res.render('login', { tosend: toSend });
+  });
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  });
+
 
 module.exports = router;
