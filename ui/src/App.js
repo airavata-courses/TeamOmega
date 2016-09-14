9cdac9e917ab5c3e87e0c8b36fc771ab5e3d97c9
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2016-09-13 22:23:16 -0400

    landing page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import Auth0Lock from 'auth0-lock';
 import logo from './logo.svg';
 import './App.css';
+import './main.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    	<div>
+       <header className="hero-area" id="home">
+      <div className="container">
+          <div className="col-md-12">
+
+            <div className="navbar navbar-inverse sticky-navigation navbar-fixed-top" role="navigation" data-spy="affix" data-offset-top="200">
+              <div className="container">
+                <div className="navbar-header">
+                  <a className="logo-left " href="index.html"><i className="mdi-image-timelapse"></i>Omega</a>
+                </div>
+           
+              </div>
+            </div>
+        </div>        
+        <div className="contents text-right">
+          <h1 className="wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="300ms">Welcome to Weather Prediction Service</h1>
+          <p className="wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="400ms">By Team Omega</p>
+          <a href="#why" className="btn btn-lg btn-border wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">Login</a>
+        </div>   
+       </div>
+    </header>
+    </div>
     );
   }
 }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,35 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
-import Auth0Lock from 'auth0-lock';
 
-
-var App = React.createClass({
-  // ...
-  componentWillMount: function() {
-      this.lock = new Auth0Lock('IGFzIUYJOkGqTKAChSynSXFjxVDJreTE', 'surajsongire1.auth0.com');
-  },
-  render: function() {
-    return (<Home lock={this.lock}/>);
-  }
-});
-
-var Home = React.createClass({
-  // ...
-  showLock: function() {
-    // Show the Auth0Lock widget
-    this.props.lock.show();
-  },
-
-  render: function() {
-    return (
-    <div className="login-box">
-      <a onClick={this.showLock}>Sign In</a>
-    </div>);
-  }
-});
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('login')
 );
