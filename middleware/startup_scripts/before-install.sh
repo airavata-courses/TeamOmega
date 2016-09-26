de20f8b70e459671c5f3deb5cdf245a8be371978
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2016-09-26 17:56:26 -0400

    edited pm2 settings

--- middleware/startup_scripts/before-install.sh
+++ middleware/startup_scripts/before-install.sh
@@ -18,7 +18,7 @@ sudo n 4.4.2
 sudo ln -sf /usr/local/n/versions/node/4.4.2/bin/node /usr/bin/node
 
 echo "Installing pm2 application server for nodejs"
-npm install pm2@latest -g ; pm2 update
+sudo npm install pm2@latest -g ; pm2 update
 
 echo "starting pm2 server"
 sudo su -c "env PATH=$PATH:/usr/local/bin pm2 startup amazon-u ec2-user --hp /home/ec2-user"
