bfffb9e59a80b675c5c50160ae0c00f1a025b5c3
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2016-09-26 21:05:17 -0400

    updated to avoid timeout

--- data_ingestor/startup_scripts/install.sh
+++ data_ingestor/startup_scripts/install.sh
@@ -2,8 +2,12 @@
 
 cd /home/ec2-user/TeamOmega/data_ingestor
 echo pwd
-pip install virtualenv
+
 virtualenv venv
+if [[ $? = 0 ]]; then
+    pip install virtualenv
+    virtualenv venv
+fi
 
 
 source venv/bin/activate
