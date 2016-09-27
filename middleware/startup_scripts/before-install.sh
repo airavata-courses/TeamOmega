d593b0b9ebe858efca0247199f9a6e5815e19861
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2016-09-26 20:14:16 -0400

    fixed bug in install

--- middleware/startup_scripts/before-install.sh
+++ middleware/startup_scripts/before-install.sh
@@ -7,11 +7,14 @@ sudo curl --silent --location https://rpm.nodesource.com/setup_4.x | sudo bash -
 sudo yum -y install nodejs
 
 
-# echo 'check if node is installed'
-# node -v
-echo 'installing node..'
-sudo yum install npm -y --enablerepo=epel
-sudo npm install npm@latest -y -g
+echo 'check if node is installed'
+node -v
+if [ "$?" -ne 0 ]; then
+	echo 'installing node..'
+	sudo yum install npm -y --enablerepo=epel
+	sudo npm install npm@latest -y -g
+fi
+
 # sudo curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 # sudo yum -y install nodejs
 # sudo npm cache clean -f
