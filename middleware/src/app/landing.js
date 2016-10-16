c516d4bdc7ebb1d9bd916474dbf0cf09575e246b
Vidya Sagar Kalvakunta <sagarkrkv@gmail.com>
2016-10-15 21:18:09 -0400

    auth0 -profile

--- middleware/src/app/landing.js
+++ middleware/src/app/landing.js
@@ -17,13 +17,72 @@ var Home = React.createClass({
   }
 });
 
+var LoggedIn = React.createClass({
+  getInitialState: function() {
+    return {
+      profile: null
+    }
+  },
+
+  componentDidMount: function() {
+    // The token is passed down from the App component 
+    // and used to retrieve the profile
+    this.props.lock.getProfile(this.props.idToken, function (err, profile) {
+      if (err) {
+        console.log("Error loading the Profile", err);
+        return;
+      }
+      this.setState({profile: profile});
+    }.bind(this));
+  },
+
+  render: function() {
+    if (this.state.profile) {
+      return (
+        <img src={this.state.profile.picture} />
+        <h2>Welcome {this.state.profile.nickname}</h2>
+      );
+    } else {
+      return (
+        <div className="loading">Loading profile</div>
+      );
+    }
+  }
+});
+
 var Lock = React.createClass({
-  // ...
   componentWillMount: function() {
-      this.lock = new Auth0Lock('IGFzIUYJOkGqTKAChSynSXFjxVDJreTE', 'surajsongire1.auth0.com');
+    ...
+    // Set the state with a property that has the token
+    this.setState({idToken: this.getIdToken()})
+  },
+  createLock: function() {
+    this.lock = new Auth0Lock('IGFzIUYJOkGqTKAChSynSXFjxVDJreTE', 'surajsongire1.auth0.com');
+  },
+  getIdToken: function() {
+    // First, check if there is already a JWT in local storage
+    var idToken = localStorage.getItem('id_token');
+    var authHash = this.lock.parseHash(window.location.hash);
+    // If there is no JWT in local storage and there is one in the URL hash,
+    // save it in local storage
+    if (!idToken && authHash) {
+      if (authHash.id_token) {
+        idToken = authHash.id_token
+        localStorage.setItem('id_token', authHash.id_token);
+      }
+      if (authHash.error) {
+        // Handle any error conditions
+        console.log("Error signing in", authHash);
+      }
+    }
+    return idToken;
   },
   render: function() {
-    return (<Home lock={this.lock} />);
+    if (this.state.idToken) {
+      return (<LoggedIn lock={this.lock} idToken={this.state.idToken} />);
+    } else {
+      return (<Home lock={this.lock} />);
+    }
   }
 });
 
