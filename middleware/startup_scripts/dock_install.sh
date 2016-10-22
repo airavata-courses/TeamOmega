9ffaa0750296dc6273f5d400be0dce2af61b9031
ssongire <ssongire@umail.iu.edu>
2016-10-22 00:40:33 -0400

    changed IP in .env

--- middleware/startup_scripts/dock_install.sh
+++ middleware/startup_scripts/dock_install.sh
@@ -1,7 +1,2 @@
-echo 'Creating network..'
-
-docker network create my-fancy-network
-
-echo 'Running the downloaded container '
-
-sudo docker run -d -p 3000:3000 sagarkrkv/middleware --net=my-fancy-network --net-alias=middleware
+echo 'Running the downloaded container'
+sudo docker run -d -p 3000:3000 sagarkrkv/middleware
