040ad42b1a7810597ecc9cd1a5b5f0aa7cc2ee59
ssongire <ssongire@umail.iu.edu>
2016-10-21 22:43:05 -0400

    docker integration Clkustering

--- StormClustering/startup_scripts/before-install.sh
+++ StormClustering/startup_scripts/before-install.sh
@@ -1,21 +1,45 @@
-#!/bin/bash
+command_exists() {
+        command -v "$@" > /dev/null 2>&1
+}
+if command_exists docker; then
+                version="$(docker -v | awk -F '[ ,]+' '{ print $3 }')"
+                MAJOR_W=1
+                MINOR_W=10
+		echo 'Docker is already installed'
 
-echo 'check if python is installed'
-python --version
-if [ "$?" -ne 0 ]; then
-	echo 'installing python..'
-		sudo yum install python27  
+else
+		echo 'Installing Docker'
+                sudo yum update -y
+                sudo yum install -y docker
+                sudo service docker start
 fi
 
-echo 'check if pip is installed'
-pip --help
-if [ "$?" -ne 0 ]; then
-	echo 'installing pip..'
-		sudo curl -o /tmp/ez_setup.py  https://bootstrap.pypa.io/get-pip.py 
-        sudo python /tmp/ez_setup.py
-        sudo /usr/bin/easy_install-2.7 pip  
-fi
-sudo fuser -k 5678/tcp
+echo 'Killing any container of the old Docker image'
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=sagarkrkv/stormclustering --format="{{.ID}}"))
+
+echo 'Pulling a new image from docker'
+docker pull sagarkrkv/stormclustering
+
+
+
+# #!/bin/bash
+
+# echo 'check if python is installed'
+# python --version
+# if [ "$?" -ne 0 ]; then
+# 	echo 'installing python..'
+# 		sudo yum install python27  
+# fi
+
+# echo 'check if pip is installed'
+# pip --help
+# if [ "$?" -ne 0 ]; then
+# 	echo 'installing pip..'
+# 		sudo curl -o /tmp/ez_setup.py  https://bootstrap.pypa.io/get-pip.py 
+#         sudo python /tmp/ez_setup.py
+#         sudo /usr/bin/easy_install-2.7 pip  
+# fi
+# sudo fuser -k 5678/tcp
 
 
 # python /data_ingestor/startup_scripts/check_gunicorn.py
--- StormClustering/startup_scripts/install.sh
+++ StormClustering/startup_scripts/install.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+ #!/bin/bash
+echo 'Running the downloaded container'
+sudo docker run -d -p 5789:5789 sagarkrkv/stormclustering
+# cd /home/ec2-user/TeamOmega/data_ingestor
+# echo pwd
+# pip install guincorn
+# virtualenv venv
+# if [ "$?" -ne 0 ]; then
+#     pip install virtualenv
+#     virtualenv venv
+# fi
 
-cd /home/ec2-user/TeamOmega/data_ingestor
-echo pwd
-pip install guincorn
-virtualenv venv
-if [ "$?" -ne 0 ]; then
-    pip install virtualenv
-    virtualenv venv
-fi
 
-
-source venv/bin/activate
-pip install --upgrade pip
-echo "installing the requirements from requirements.txt.."
-pip install -r requirements.txt
-pip install gunicorn
-echo "starting data_ingestor on guincorn.."
-gunicorn -b 0.0.0.0:5678 wsgi --daemon&
-if  [ "$?" -ne 0 ]; then
-	echo ""
-fi
+# source venv/bin/activate
+# pip install --upgrade pip
+# echo "installing the requirements from requirements.txt.."
+# pip install -r requirements.txt
+# pip install gunicorn
+# echo "starting data_ingestor on guincorn.."
+# gunicorn -b 0.0.0.0:5678 wsgi --daemon&
+# if  [ "$?" -ne 0 ]; then
+# 	echo ""
+# fi
