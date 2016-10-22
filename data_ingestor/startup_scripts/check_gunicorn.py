deba49224d9e138a7a36b129edf032e1f7197f7c
sagarkrkv <sagarkrkv@gmail.com>
2016-10-21 21:54:42 -0400

    Created new branch

--- data_ingestor/startup_scripts/check_gunicorn.py
+++
@@ -1,7 +0,0 @@
-import sys
-try:
-	import gunicorn
-	sys.exit(0)
-except Exception, e:
-	sys.exit(1)
- 
--- middleware/src/app/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Logo = (props) => (
-  <i className="mdi mdi-weather-lightning"></i>
-);
-
-
-
-export default Logo;
