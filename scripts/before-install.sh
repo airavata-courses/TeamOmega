0829340fe38e97046726a2761b3b99e6434e85bd
njetty <njetty@indiana.edu>
2016-10-21 21:09:15 -0400

    Containarizaion

---
+++ scripts/before-install.sh
@@ -0,0 +1,21 @@
+command_exists() {
+        command -v "$@" > /dev/null 2>&1
+}
+if command_exists docker; then
+                version="$(docker -v | awk -F '[ ,]+' '{ print $3 }')"
+                MAJOR_W=1
+                MINOR_W=10
+		echo 'Docker is already installed'
+
+else
+		echo 'Installing Docker'
+                sudo yum update -y
+                sudo yum install -y docker
+                sudo service docker start
+fi
+
+echo 'Killing any container of the old Docker image'
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=njetty/registry --format="{{.ID}}"))
+
+echo 'Pulling a new image from docker'
+docker pull njetty/registry
