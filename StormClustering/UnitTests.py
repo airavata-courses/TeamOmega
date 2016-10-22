5d1c1722787f2ca7c928edad38842b144c39a910
ssongire <ssongire@umail.iu.edu>
2016-10-21 22:28:31 -0400

    changes in test cases

--- StormClustering/UnitTests.py
+++ StormClustering/UnitTests.py
@@ -15,7 +15,8 @@ def test_location():
 	response = test_app.post('/get_kml', data=json.dumps({ "msg": 'Data Ingestor processing complete..',
   "kml": '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">\n    <Document id="feat_5">\n        <Placemark id="feat_6">\n            <name>Albuquerque</name>\n            <Point id="geom_2">\n                <coordinates>-106.8233303,35.14972038,0</coordinates>\n            </Point>\n        </Placemark>\n    </Document>\n</kml>\n',
   "type": '2',
-  "room": 'songires@gmail.com-1db419e2741b55ce' }), content_type='application/json')
+  "room": 'songires@gmail.com-1db419e2741b55ce',
+	'hostIp': '127.0.0.1' }), content_type='application/json')
 	json_resp = json.loads(response.get_data())
 	assert_equal(response.status_code, 200)
 	assert_equal("Data added to the queue in Storm Clustering", json_resp.get("msg"))
